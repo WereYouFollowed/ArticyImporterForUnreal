40414fe376b2b8300ca75306546614f2c69f8d64
Adam Parkinson <salamiarmi@gmail.com>
2025-12-21 07:08:27 +0000

    - compile fix for ue 5.6.

--- Source/ArticyRuntime/Public/Interfaces/ArticyFlowObject.h
+++ Source/ArticyRuntime/Public/Interfaces/ArticyFlowObject.h
@@ -15,7 +15,7 @@ class UArticyFlowObject : public UArticyReflectable { GENERATED_BODY() };
  * All objects that are part of a flow (i.e. can be traversed by the Flow Player) need
  * to implement this interface.
  */
-class IArticyFlowObject
+class IArticyFlowObject : public IArticyReflectable
 {
 	GENERATED_BODY()
 
